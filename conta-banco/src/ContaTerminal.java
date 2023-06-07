fcc398b95434c9467be29544bef927ebc8a00386
Rapadura-gb <gabrielantoniazzi7@gmail.com>
2023-06-06 23:26:51 -0300

    Pedit inputs utilizando o scanner e armazenar nas suas respectivas vairaveis

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        //create a scanner
+        //criar Scanner
         Scanner sc = new Scanner(System.in);
 
+        //Pedir  inputs do úsuario e armazenar os valores em suas respectivas variaveis
+        System.out.println("Por favor, digite seu nome: ");
+        String nomeCliente = sc.nextLine();
+        
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        //resolvendo o problema do nextInt() não pegar a linha inteira assim pulando o nextLine()
+        int numero = Integer.parseInt(sc.nextLine());
+       
+
+        System.out.println("Por favor, digite o número da agencia: ");
+        String agencia = sc.nextLine();
+        
+
+
+        System.out.println("Por favor, digite o valor que gostaria de depositar: ");
+        double saldo = sc.nextDouble();
+        
 
     }
 }
