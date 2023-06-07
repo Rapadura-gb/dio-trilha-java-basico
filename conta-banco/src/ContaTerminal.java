acc4d8b1d63b9e8b2f64dc5f36b25e649926c08e
Rapadura-gb <gabrielantoniazzi7@gmail.com>
2023-06-06 22:50:37 -0300

    Import and create a Scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //create a scanner
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
